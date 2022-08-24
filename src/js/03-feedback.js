ae2d6dfde89e1bf0f5f1a92524e4d253e4cba648
Aleksandr Shkola <sprinterr23@gmail.com>
2022-08-24 22:19:56 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,7 +1,6 @@
 import throttle from 'lodash.throttle';
 
 const STORAGE_KEY = 'feedback-form-state';
-const formData = {};
 
 const form = document.querySelector('.feedback-form');
 
@@ -11,26 +10,25 @@ form.addEventListener('submit', onFormSubmit);
 populateFormOutput();
 
 function onFormInput(e) {
-  formData[e.target.name] = e.target.value;
-
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  let savedMessage = localStorage.getItem(STORAGE_KEY);
+  savedMessage = savedMessage ? JSON.parse(savedMessage) : {};
+  savedMessage[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(savedMessage));
 }
 
 function onFormSubmit(e) {
   e.preventDefault();
+  console.log(localStorage.getItem(STORAGE_KEY));
   e.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
 }
 
 function populateFormOutput() {
-  const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  let savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
 
   if (savedMessage) {
     Object.entries(savedMessage).forEach(([name, value]) => {
       form.elements[name].value = value;
-      formData[name] = value;
     });
   }
 }
-
-console.log('Object', formData);
